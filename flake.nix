6f4bcb8a089cfa8bc5f0972705b13eb3545ee120
Francis Begyn <francis@begyn.be>
2024-08-24 13:53:25 +0200

    fix the nix development environment
    Signed-off-by: Francis Begyn <francis@begyn.be>

--- flake.nix
+++ flake.nix
@@ -6,38 +6,50 @@
       url = "github:edolstra/flake-compat";
       flake = false;
     };
+    devshell = {
+      url = "github:numtide/devshell";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
   };
 
   outputs = {
     self,
     nixpkgs,
     flake-utils,
+    devshell,
     ...
   } @inputs:
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = import nixpkgs {
         inherit system;
+        overlays = [ devshell.overlays.default ];
       };
     in rec {
-      devShells.default = pkgs.mkShell rec {
-        buildInputs = [
-          pkgs.aptly
-          pkgs.nfpm
-          pkgs.git
-          pkgs.nix
-          pkgs.curl
-          pkgs.envsubst
-          pkgs.busybox
-        ];
-        commands = [
-          {
-            name = "fbd-package:fetch";
-            category = "download";
-            help = "downloads the packages for the exporters";
-            command = ''
-            '';
-          }
-        ];
+      devShells = rec {
+        default = fbdPackages;
+        fbdPackages = pkgs.devshell.mkShell rec {
+          name = "fbd-packages";
+          packages = [
+            pkgs.aptly
+            pkgs.nfpm
+            pkgs.git
+            pkgs.nix
+            pkgs.curl
+            pkgs.envsubst
+          ];
+          commands = [
+            {
+              name = "fbd-package:package";
+              category = "package";
+              help = "downloads the packages for the exporters";
+              command = ''
+                ./fbd-gh-package.sh
+              '';
+            }
+          ];
+        };
       };
     });
 }
